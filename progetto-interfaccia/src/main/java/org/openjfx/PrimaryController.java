bfc6d988bcffdf639756c10a726d7877e7ff1e2b
lorenzobruni-dev <lorenzobruni40@gmail.com>
2022-10-22 11:42:54 +0200

    Prima Pagina
    Istanziata la prima pagina

---
+++ progetto-interfaccia/src/main/java/org/openjfx/PrimaryController.java
@@ -0,0 +1,11 @@
+package org.openjfx;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void apriSezioneOperatoreSanitario() throws IOException {
+    }
+}
