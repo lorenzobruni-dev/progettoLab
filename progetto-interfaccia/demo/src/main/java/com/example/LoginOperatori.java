84ef0a3c438f1dbe72fa706bf84b5457d8f15082
lorenzobruni-dev <lorenzobruni40@gmail.com>
2022-11-06 17:45:49 +0100

    Logica Login
    Istanziata la logica del login
    N.B => da istanziare il db

--- progetto-interfaccia/demo/src/main/java/com/example/LoginOperatori.java
+++ progetto-interfaccia/demo/src/main/java/com/example/LoginOperatori.java
@@ -1,6 +1,8 @@
 package com.example;
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import url.urlConnessione;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,13 +11,35 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class LoginOperatori {
-
+    String userCredential = "admin";
+    String passwordCredential = "admin";
+    boolean loginConfirm = false;
+    @FXML
+    Label CheckPassword = new Label();
+    @FXML
+    TextField usernameOperatore = new TextField();
+    @FXML
+    TextField password = new TextField();
     @FXML
     private void sceltaOp() throws IOException {
+        
+        String user = usernameOperatore.getText();
+        String pwd = password.getText();
+        System.out.println("Username: " + user + " - " + "Password : " + pwd);
+        loginConfirm = validateLogin(user,pwd);
+        if(loginConfirm == true)
         App.setRoot("SceltaOperatore");
+        else 
+        CheckPassword.setVisible(true);
     }
     @FXML
     private void backToHub() throws IOException {
         App.setRoot("HubIniziale");
+    }
+    private boolean validateLogin(String user, String pwd)
+    {
+        if((user == userCredential)&&(pwd == passwordCredential)||(user!="" || pwd !=""))
+        return true;
+        else return false;
     }
 }
