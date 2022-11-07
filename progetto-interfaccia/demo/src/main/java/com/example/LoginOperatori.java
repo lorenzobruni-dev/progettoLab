eaab5dd9bf2795f11a9ad214beb1c5761937b1a2
lorenzobruni-dev <lorenzobruni40@gmail.com>
2022-11-07 23:00:49 +0100

    Login corretto
    Terminata logica => fixato errore login validateLogin()
    - Da implementare la jdbc

--- progetto-interfaccia/demo/src/main/java/com/example/LoginOperatori.java
+++ progetto-interfaccia/demo/src/main/java/com/example/LoginOperatori.java
@@ -27,7 +27,7 @@ public class LoginOperatori {
         String pwd = password.getText();
         System.out.println("Username: " + user + " - " + "Password : " + pwd);
         loginConfirm = validateLogin(user,pwd);
-        if(loginConfirm == true)
+        if(loginConfirm)
         App.setRoot("SceltaOperatore");
         else 
         CheckPassword.setVisible(true);
@@ -37,8 +37,11 @@ public class LoginOperatori {
         App.setRoot("HubIniziale");
     }
     private boolean validateLogin(String user, String pwd)
-    {
-        if((user == userCredential)&&(pwd == passwordCredential)||(user!="" || pwd !=""))
+    {   
+        boolean ex1 = userCredential.equals(user);
+        boolean ex2 = passwordCredential.equals(pwd);
+        boolean ex3 = ex1 && ex2;
+        if(userCredential.equals(user) && passwordCredential.equals(pwd))
         return true;
         else return false;
     }
