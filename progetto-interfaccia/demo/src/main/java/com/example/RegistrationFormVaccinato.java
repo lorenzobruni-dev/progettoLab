516fbe8462cdff26b22b22c6fdcae3e618804cf5
lorenzobruni-dev <109805268+lorenzobruni-dev@users.noreply.github.com>
2023-01-03 16:53:17 +0100

    JavaDoc RegistrationFormVaccinato

--- progetto-interfaccia/demo/src/main/java/com/example/RegistrationFormVaccinato.java
+++ progetto-interfaccia/demo/src/main/java/com/example/RegistrationFormVaccinato.java
@@ -4,6 +4,8 @@
  * LUTSYSHYNA ANNA - MATRICOLA 745509 - VA
  * PANARESE ALESSIO - MATRICOLA 750887 - VA
  */
+
+//package globale com.example
 package com.example;
 
 import java.io.IOException;
@@ -29,71 +31,118 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 
+    /**
+    Classe che specifica la form di registrazione del vaccinato ad un dato centro
+    @author Team
+    @see RegistrationFormVaccinato
+    */
 public class RegistrationFormVaccinato implements Initializable {
 
+    /**
+    Metodo che permette di far tornare indietro alla SceltaOperatore
+    @see RegistrationFormVaccinato
+    */
     @FXML
     private void backToHub() throws IOException {
         App.setRoot("SceltaOperatore");
     }
 
-    boolean controlloCampoDatiVaccinato = false;
-    boolean checkPresenzaDB = false;
-    Alert a = new Alert(AlertType.INFORMATION);
+    boolean controlloCampoDatiVaccinato = false; //variabile di controllo sui campi del vaccinato
+    boolean checkPresenzaDB = false; //variabile booleana che controlla se a DB è presente il centroSelezionato
+    Alert a = new Alert(AlertType.INFORMATION); //alert di information che avvisa che il Vaccinato è stato registrato con successo
 
+    /** @param TextField textfield dedicata al nome del vaccinato*/
     @FXML
     TextField nomeVaccinato;
+
+    /** @param TextField textfield dedicata al cognome del vaccinato*/
     @FXML
     TextField cognomeVaccinato;
+
+    /** @param TextField textfield dedicata al codice fiscale del vaccinato*/
     @FXML
     TextField cfVaccinato;
+
+    /** @param TextField textfield dedicata alla visualizzazione dell'id univoco del vaccinato*/
     @FXML
     TextField idUnivocoVaccinato;
+
+    /** @param TextField textfield dedicata alla visualizzazione del nome del centro vaccinale*/
     @FXML
     TextField nomeCentroVaccinale;
+
+    /** @param TextField textfield dedicata alla visualizzazione del comune del centro vaccinale*/
     @FXML
     TextField comuneCentroVaccinale;
+
+     /** @param TextField textfield dedicata alla visualizzazione del cap del centro vaccinale*/
     @FXML
     TextField capCentroVaccinale;
+
+     /** @param TextField textfield dedicata alla visualizzazione dell'indirizzo del centro vaccinale*/
     @FXML
     TextField indirizzoCentroVaccinale;
+
+     /** @param TextField textfield dedicata alla visualizzazione del civico del centro vaccinale*/
     @FXML
     TextField CivicoCentro;
 
+     /** @param DatePicker datepicker dedicata alla visualizzazione della data di nascita del vaccinato*/
     @FXML
     DatePicker dataVaccinato;
+    
+    /** @param DatePicker datepicker dedicata alla visualizzazione della data di somministrazione del vaccinato*/
     @FXML
     DatePicker dataSommVaccinato;
 
+    /** @param ComboBox combobox dedicata alla visualizzazione del genere del vaccinato*/
     @FXML
     ComboBox<String> genereVaccinato;
+
+    /** @param ComboBox combobox dedicata alla visualizzazione del genere del vaccinato*/
     @FXML
     ComboBox<String> nomeVaccino;
+
+    /** @param ComboBox combobox dedicata alla visualizzazione della provincia del centro*/
     @FXML
     ComboBox<SigleProvince> provinciaCentroVaccinale;
+
+    /** @param ComboBox combobox dedicata alla visualizzazione della tipologia del centro*/
     @FXML
     ComboBox<TipoCentro> tipologiaCentroVaccinale;
+
+    /** @param ComboBox combobox dedicata alla visualizzazione della via del centro*/
     @FXML
     ComboBox<Qualificatore> ViaCentro;
 
+    /** @param Label label che informa se ci sono stati errori in fase di inserimento*/
     @FXML
     Label checkCampi;
+
+    /** @param Button bottone di registrazione*/
     @FXML
     Button RegistraVaccinato;
 
-    
+    /**
+    Metodo che permette di registrare un vaccinato
+    @see RegistrationFormVaccinato
+    */
     @FXML
     private void registraVaccinato() throws IOException {
 
-        ArrayList<String> datiVaccinato = getDati();
-        ArrayList<SigleProvince> datoSigla = getDatiProvincia();
-        ArrayList<Qualificatore> datiQualificatore = getDatoQualificatore();
-        ArrayList<TipoCentro> datiTipoCentro = getDatiTipoCentro();
+        ArrayList<String> datiVaccinato = getDati(); // ArrayList contenente tutti i dati di registrazione
+        ArrayList<SigleProvince> datoSigla = getDatiProvincia(); //ArrayList contenente la sigla della provincia
+        ArrayList<Qualificatore> datiQualificatore = getDatoQualificatore(); //ArrayList contenente il qualificatore della via del centro
+        ArrayList<TipoCentro> datiTipoCentro = getDatiTipoCentro(); //ArrayList contenente il tipo di centro
 
-        ArrayList <CentroVaccinale> datiCentroDB = new ArrayList<>();
-        ArrayList <String> controlloDatiCentro = new ArrayList<>();
+        ArrayList <CentroVaccinale> datiCentroDB = new ArrayList<>(); //ArrayList contenente i dati del centro , presente a DB
+        ArrayList <String> controlloDatiCentro = new ArrayList<>();  //ArrayList temporanea utile per il controllo della presenza di un centro a DB
 
             
         try{
+
+            //controllo di dati non compilati
+
             if(datiVaccinato.get(0).equals("") || 
             datiVaccinato.get(1).equals("") || 
             datiVaccinato.get(2).equals("-" + "-" + "-") ||
@@ -122,13 +171,15 @@ public class RegistrationFormVaccinato implements Initializable {
                 //chiamata DB per getDatiCentro
                 datiCentroDB = istanzaServer.server.getCentriVaccinali();
                 
+                // metodo che controlla la presenza a DB -> valore ritorno il checkPresenzaDB il quale verifica la presenza a DB
                 checkPresenzaDB = validateCentro(controlloDatiCentro,datiCentroDB,datoSigla,datiQualificatore,datiTipoCentro);
-    
+            
+            //if che abilita alla scrittura a DB dei dati del Vaccinato
             if(controlloCampoDatiVaccinato || !checkPresenzaDB)
                 checkCampi.setVisible(true);
             else {
-                istanzaServer.server.createTableDinamica(datiVaccinato);
-                istanzaServer.server.setVaccinato(datiVaccinato);
+                istanzaServer.server.createTableDinamica(datiVaccinato); //chiamata DB per createTableDinamica
+                istanzaServer.server.setVaccinato(datiVaccinato); //chiamata DB per insert data
                 a.setHeaderText("Registrato con successo!");
                 a.setContentText(datiVaccinato.get(7).toString());
                 a.show();
@@ -142,6 +193,12 @@ public class RegistrationFormVaccinato implements Initializable {
             System.out.println(e);
         }
     }
+
+    /**
+    Metodo che permette di validare il centro presente a DB con i dati del centro appena inseriti
+    @see RegistrationFormVaccinato
+    */
+
     private boolean validateCentro(ArrayList<String> controlloDatiCentro, ArrayList<CentroVaccinale> datiCentroDB, ArrayList<SigleProvince> datoSigla, ArrayList<Qualificatore> datiQualificatore, ArrayList<TipoCentro> datiTipoCentro) {
         
         if(!datiCentroDB.isEmpty()){
@@ -160,6 +217,10 @@ public class RegistrationFormVaccinato implements Initializable {
         return checkPresenzaDB;
     }
 
+    /**
+    Metodo che permette di inserire i dati del tipo centro in un ArrayList
+    @see RegistrationFormVaccinato
+    */
     public ArrayList <TipoCentro> getDatiTipoCentro(){
 
         ArrayList<TipoCentro> datiTemp = new ArrayList<>();
@@ -167,6 +228,10 @@ public class RegistrationFormVaccinato implements Initializable {
         datiTemp.add(tipologiaCentroVaccinale.getValue());
         return datiTemp;
     }
+    /**
+    Metodo che permette di inserire i dati del qualificatore della via del centro in un ArrayList
+    @see RegistrationFormVaccinato
+    */
     public ArrayList<Qualificatore> getDatoQualificatore() {
 
         ArrayList<Qualificatore> datiTemp = new ArrayList<>();
@@ -174,6 +239,10 @@ public class RegistrationFormVaccinato implements Initializable {
         datiTemp.add(ViaCentro.getValue());
         return datiTemp;
     }
+    /**
+    Metodo che permette di inserire i dati della provincia del centro in un ArrayList
+    @see RegistrationFormVaccinato
+    */
     public ArrayList<SigleProvince> getDatiProvincia() {
 
         ArrayList<SigleProvince> dati = new ArrayList<>();
@@ -181,6 +250,10 @@ public class RegistrationFormVaccinato implements Initializable {
         dati.add(provinciaCentroVaccinale.getValue());
         return dati;
     }
+    /**
+    Metodo che permette di inserire i dati del centro in un ArrayList
+    @see RegistrationFormVaccinato
+    */
     public ArrayList<String> getDati() {
 
         ArrayList<String> datiTemp = new ArrayList<>();
@@ -211,6 +284,10 @@ public class RegistrationFormVaccinato implements Initializable {
         return datiTemp;
     }
 
+    /**
+    Metodo che permette di generare l'id univoco di registrazione
+    @see RegistrationFormVaccinato
+    */
     private String generazioneIDUnivoco() {
         String carId = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String randString = " ";
@@ -224,6 +301,10 @@ public class RegistrationFormVaccinato implements Initializable {
         return randString;
     }
 
+     /**
+    Metodo che permette di istanziare le varie combobox con gli opportuni campi
+    @see RegistrationFormVaccinato
+    */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         genereVaccinato.setItems(FXCollections.observableArrayList("Male", "Female", "Altro/a", "MUCCA"));
